2bf485e42a0ab886cfae1627876dac7fc4185b83
yx <yangxue.cs@foxmail.com>
2023-02-25 19:04:56 +0800

    add some submodule

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,15 @@
+;;; init.el --- emacs init.el. -*- coding: utf-8; lexical-binding: t; -*-
+;;; Code:
+(require 'init-utils)
+(require 'init-basic)
+(require 'init-keymaps)
+(require 'init-completion)
+(require 'init-evil)
+(require 'init-misc)
+(require 'init-notes)
+(require 'init-mail)
+(require 'init-elfeed)
+(require 'init-ide)
+(require 'init-lang)
+
+;;; init.el ends here
