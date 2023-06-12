4571587db621568b3b1e76292a5c44ca99ba6e22
yx <yangxue.cs@foxmail.com>
2023-06-12 14:26:50 +0800

    init-feed

---
+++ emacs/.emacs.d/lisp/init-feed.el
@@ -0,0 +1,11 @@
+(use-package elfeed
+  :init
+  (setq elfeed-feeds
+        '(("https://ruder.io/rss/index.rss" ai nlp)
+          ("https://www.inference.vc/rss" ai)
+          ("https://emacsredux.com/atom.xml" emacs)
+          ("https://sachachua.com/blog/category/emacs/feed/" emacs)
+          ("https://manateelazycat.github.io/feed.xml" emacs)
+          ("http://www.ruanyifeng.com/blog/atom.xml" 技术 软件))
+        )
+  )
--- emacs/.emacs.d/lisp/init-misc.el
+++ emacs/.emacs.d/lisp/init-misc.el
@@ -175,18 +175,5 @@
     :init (pyim-basedict-enable))
   )
 
-(use-package elfeed
-  :init
-  (setq elfeed-feeds
-        '(("https://ruder.io/rss/index.rss" ai nlp)
-          ("https://www.inference.vc/rss" ai)
-          ("https://emacsredux.com/atom.xml" emacs)
-          ("https://sachachua.com/blog/category/emacs/feed/" emacs)
-          ("https://manateelazycat.github.io/feed.xml" emacs)
-          ("https://linuxhandbook.com/rss/" linux)
-          ("http://www.ruanyifeng.com/blog/atom.xml" 技术 软件))
-        )
-  )
-
 (provide 'init-misc)
 ;;; init-misc.el ends here
