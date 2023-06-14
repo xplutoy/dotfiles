0097b455780c4e4ef45729a6c36b1b3dc875a697
yx <52785889+xplutoy@users.noreply.github.com>
2023-06-14 14:51:12 +0800

    Update init-feed.el

--- emacs/.emacs.d/lisp/init-feed.el
+++ emacs/.emacs.d/lisp/init-feed.el
@@ -1,12 +1,19 @@
 (use-package elfeed
   :init
   (setq elfeed-feeds
-        '(("https://ruder.io/rss/index.rss" ai nlp)
-          ("https://www.inference.vc/rss" ai)
-          ("https://emacsredux.com/atom.xml" emacs)
+        '(
+          ;; 工具、技术
           ("https://sachachua.com/blog/category/emacs/feed/" emacs)
           ("https://manateelazycat.github.io/feed.xml" emacs)
-          ("http://www.ruanyifeng.com/blog/atom.xml" 技术 软件))
+          ("http://www.ruanyifeng.com/blog/atom.xml" 技术 软件)
+          
+          ;; 数据、R、Python、Julia等
+          
+          ;; 科研、数学、物理
+          ("https://ruder.io/rss/index.rss" ai nlp)
+          ("https://www.inference.vc/rss" ai)
+          ("https://lilianweng.github.io/index.xml" AI)
+          )
         )
   )
 
