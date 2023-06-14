edb82aa28c4cb06d136dfd12905926b8f8e1e32a
yx <yangxue.cs@foxmail.com>
2023-06-14 15:14:28 +0800

    clean feed

--- emacs/.emacs.d/lisp/init-feed.el
+++ emacs/.emacs.d/lisp/init-feed.el
@@ -1,17 +1,14 @@
 (use-package elfeed
   :init
   (setq elfeed-feeds
-        '(
-          ;; 工具、技术
+        '(;; 工具、技术
           ("https://sachachua.com/blog/category/emacs/feed/" emacs)
           ("https://manateelazycat.github.io/feed.xml" emacs)
-          ("http://www.ruanyifeng.com/blog/atom.xml" 技术 软件)
-          
+          ("http://www.ruanyifeng.com/blog/atom.xml" TECH)
           ;; 数据、R、Python、Julia等
-          
           ;; 科研、数学、物理
-          ("https://ruder.io/rss/index.rss" ai nlp)
-          ("https://www.inference.vc/rss" ai)
+          ("https://ruder.io/rss/index.rss" AI)
+          ("https://www.inference.vc/rss" AI)
           ("https://lilianweng.github.io/index.xml" AI)
           )
         )
