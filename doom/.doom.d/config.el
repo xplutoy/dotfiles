1afd2a3ca3db2d40681bd8b79399dcac22c883f8
yx <yangxue.cs@foxmail.com>
2024-03-13 12:39:29 +0800

    add doom

---
+++ doom/.doom.d/config.el
@@ -0,0 +1,11 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+(setq user-full-name "Yang Xue"
+      user-mail-address "yangxue.wk@foxmail.com")
+
+(setq doom-theme 'doom-one)
+
+(prefer-coding-system 'utf-8-unix)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+(setq doom-font (font-spec :family "JetBrains Mono NL" :size 14.0))
